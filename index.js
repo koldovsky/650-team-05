ecaa9ab15f094a9d2666c2f41e2b76c6fa31fc1b
Andriy Mriy <AndriyMriy@gmail.com>
2021-11-05 17:43:53 +0200

    fix 398 and 52 line

--- index.js
+++ index.js
@@ -16,21 +16,21 @@ function updateCountDown() {
     const m = Math.floor(diff / 1000 / 60) % 60;
     const s = Math.floor(diff / 1000) % 60;
 
-    days.innerHTML = d;
-    hours.innerHTML = h < 10 ? "0" + h : h;
-    minutes.innerHTML = m < 10 ? "0" + m : m;
-    seconds.innerHTML = s < 10 ? "0" + s : s;
-
+    // days.innerHTML = d;
+    // hours.innerHTML = h < 10 ? "0" + h : h;
+    // minutes.innerHTML = m < 10 ? "0" + m : m;
+    // seconds.innerHTML = s < 10 ? "0" + s : s;
+    console.log(d);
 }
 
 setInterval(updateCountDown, 1000)
 // --------------------------------------- countdown --------------------------------------- 
 
 // --------------------------------------- map --------------------------------------- 
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(51.508742, -0.120850),
-        zoom: 5,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+// function myMap() {
+//     var mapProp = {
+//         center: new google.maps.LatLng(51.508742, -0.120850),
+//         zoom: 5,
+//     };
+//     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+// }
