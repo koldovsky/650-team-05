eeb47d70f6659c762caf41a7fae310be4d4890b6
MariiaYaskiv <lytvyn.mariia@gmail.com>
2021-11-29 15:45:16 +0200

    added js code to block testimonials

--- js/testimonials-testimonials.js
+++ js/testimonials-testimonials.js
@@ -12,11 +12,15 @@
         const slidesContainer = document.querySelector(`.carousel .slides-container`);
         slidesContainer.innerHTML = slides[currentSlideIdx];
         if (window.innerWidth > 600) {
-            const slide2Idx = carrentSlideIdx + 1 >= slides.length ? 0 : carrentSlideIdx + 1;
+            const slide2Idx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
             slidesContainer.innerHTML += slides[slide2Idx];
             if (window.innerWidth > 800) {
                 const slide3Idx = slide2Idx + 1 >= slides.length ? 0 : slide2Idx + 1;
                 slidesContainer.innerHTML += slides[slide3Idx];
+                if (window.innerWidth > 767) {
+                    const slide2Idx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
+                    slidesContainer.innerHTML += slides[slide2Idx];
+                }
             }
         }
     }
